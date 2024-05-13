cc790f7a20f267469ce18c4c31ed1fc8e017ccf2
root <root@Moctinet>
2024-05-13 17:48:33 +0200

    update Lists of lists = Matrix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for num in range(len(row)):
             print("{:d}".format(row[num]), end=" ")
-            if num != len(row) -1:
-                print(" ",end="")
-        print()
+            if num != len(row) - 1:
+                print(" ", end=" ")
+                print()
