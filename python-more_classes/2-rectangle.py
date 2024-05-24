831a99d76671fd57cc28adcb72936bcaead3bf47
Moctar93 <moctartize93@gmail.com>
2024-05-24 23:30:33 +0200

    Area and Perimeter

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -7,9 +7,9 @@
 
 
 class Rectangle:
-"""
-    Class Rectangle calculate area and perimeter
-"""
+    """
+        Rectangle class
+    """
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
