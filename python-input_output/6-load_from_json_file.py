174098d22d60c70d876b282e462b96d9f3f4daee
root <root@Moctinet>
2024-05-27 15:55:33 +0200

    function that creates an Object from a “JSON file”

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -7,11 +7,11 @@ import json
 def load_from_json_file(filename):
     """
     Creates an object from a JSON file.
-    
+
     Args:
         filename: The name of the JSON file to load.
-        
-    Returns:
+
+        Returns:
         The object represented by the JSON data in the file.
     """
     with open(filename, 'r') as f:
