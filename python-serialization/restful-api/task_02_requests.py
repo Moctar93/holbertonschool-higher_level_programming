0e2ddc75924727d1af78540898c2f070ddb3fd63
root <root@Moctinet>
2024-05-31 23:44:51 +0200

    Consuming and processing data from an API using Python

--- python-serialization/restful-api/task_02_requests.py
+++ python-serialization/restful-api/task_02_requests.py
@@ -4,27 +4,25 @@ import csv
 
 
 def fetch_and_print_posts():
-    response = requests.get('https://jsonplaceholder.typicode.com/posts')
-    print(f'Status Code: {response.status_code}')
+    response = requests.get("https://jsonplaceholder.typicode.com/posts")
+    print(f"Status Code: {response.status_code}")
 
     if response.status_code == 200:
-        data = response.json()
-        for dd in data:
-            print(dd['title'])
+        posts = response.json()
+        for post in posts:
+            print(post['title'])
 
 
 def fetch_and_save_posts():
-    response = requests.get('https://jsonplaceholder.typicode.com/posts')
-    if response.status_code == 200:
-        with open('posts.csv', 'w', newline='') as f:
-            writer = csv.writer(f)
-            fields = ['id', 'title', 'body']
+    response = requests.get("https://jsonplaceholder.typicode.com/posts")
 
-            writer.writerow(fields)
-            data = response.json()
-            for dd in data:
-                writer.writerow([
-                    dd['id'],
-                    dd['title'],
-                    dd['body']
-                ])
+    if response.status_code == 200:
+        posts = response.json()
+        structured_data = [{'id': post['id'], 'title': post['title'], 'body': post['body']} for post in posts]
+
+        with open('posts.csv', 'w', newline='') as csvfile:
+            fieldnames = ['id', 'title', 'body']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            writer.writeheader()
+            writer.writerows(structured_data)
